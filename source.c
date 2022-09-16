792f1f4a4e4fbbfcccffc921a5a2316c99f8cd82
hvitmid <112760988+hvitmid@users.noreply.github.com>
2022-09-16 10:13:53 +0300

    Update source.c

--- source.c
+++ source.c
@@ -5,8 +5,10 @@ void main(void)
 {
 	setlocale(LC_ALL, "RUS");
 	int n = 4, k = 2, m = 6;
-	printf("%d\n\t",n);
-	printf("число %d, %d - цифр целой части и %d цифр дробной части\n\n", n, k, m );
-	printf("%2.6f", n/333.);
+	printf("число %d, %d - цифр целой части и %d цифр дробной части\n\n", n, k, m);
+	printf("%d\n", 333);
+	printf("%s\n","___________");
+	printf("%s\n\t","ответ - ");
+	printf("%+2.6f", n/333.);
 	
 }
